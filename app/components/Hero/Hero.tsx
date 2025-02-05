18e90abe67226ddcd65586c074992e1523ed6aca
thehashton <harry.ashton.uk@gmail.com>
2025-02-05 11:33:27 +0000

    7. Changes hero

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import scss from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <Paper className={scss.Hero} elevation={5}>
-      <Typography>Welcome to Curious Courses</Typography>
+    <Paper className={scss.Hero} variant={"elevation"} elevation={5}>
+      <Typography variant="h1" fontSize={30} fontWeight={'bold'}>Welcome to Curious Courses</Typography>
+      <Typography variant="h2" fontSize={20}>Frontend courses that will supercharge your learning!</Typography>
+      <Button variant="contained">View Courses</Button>
     </Paper>
   );
 };
